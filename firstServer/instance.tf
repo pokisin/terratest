9a278d801ef0628b94047663ba1c9f9d1c945a87
Wilmar Pacheco <sfil.rojas@gmail.com>
2022-03-23 13:58:12 -0600

    fixed configuration instance.tf

--- firstServer/instance.tf
+++ firstServer/instance.tf
@@ -7,10 +7,13 @@ terraform {
     }
 }
 
-provider "aws" { 
-    access_key = "xxxxxxxxxxxxxxxx" 
-    secret_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    region = "us-east-1" 
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_KEY_ID" {}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = var.AWS_ACCESS_KEY_ID 
+    secret_key = var.AWS_SECRET_KEY_ID
 }
 
 resource "aws_instance" "hello-instance" {
